9a73a73b40e14b188e4c0d75e4b78646951ada31
Vincent BAUDIN <vbaudin@e2r7p23.42.fr>
2018-01-10 11:45:49 +0100

    d01/ex04 finish

---
+++ d01/ex04/ex04.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex04.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbaudin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/10 11:33:49 by vbaudin           #+#    #+#             */
+/*   Updated: 2018/01/10 11:33:51 by vbaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+  std::string		string = "HI THIS IS BRAIN";
+	std::string   *pointer = &string;
+	std::string   &reference = string;
+
+	std::cout << *pointeur << std::endl;
+	std::cout << reference << std::endl;
+}
